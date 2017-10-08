1083284ee984592b599d2853cceab9510f91260d
bernardoleite <up201404464@gcloud.fe.up.pt>
2017-10-08 18:49:40 +0100

    Better House (Changed Cylinder and Sphere)

--- project1/parser/Cylinder.js
+++ project1/parser/Cylinder.js
@@ -1,108 +1,93 @@
- /**
+/*
  * Cylinder
  * @constructor
+ * @param scene
+ * @param attributes [height, bottomRadius, topRadius, slices, stacks]
  */
-
- function Cylinder(scene, height, bottomRadius, topRadius, stacks, slices,  texture) {
+ function Cylinder(scene, height, bottomRadius, topRadius, stacks, slices) {
  	CGFobject.call(this,scene);
-	top = typeof top !== 'undefined' ? top : false;
-	bottom = typeof bottom !== 'undefined' ? bottom : false;
-	texture = typeof texture !== 'undefined' ? texture : false;
-	this.top = top;
-	this.bottom = bottom;
-	this.texture = texture;
-	this.topRad = topRadius;
-	this.bottomRad = bottomRadius;
+
 	this.height = height;
-	this.slices=slices;
-	this.stacks=stacks;
-	this.type = 'cylinder';
+	this.bottomRadius = bottomRadius;
+	this.topRadius = topRadius;
+	this.stacks = stacks;
+	this.slices = slices;
+
+	this.angle=2*Math.PI/this.slices;
 
  	this.initBuffers();
  };
 
  Cylinder.prototype = Object.create(CGFobject.prototype);
+
  Cylinder.prototype.constructor = Cylinder;
 
  Cylinder.prototype.initBuffers = function() {
-	 this.vertices = [];
-	 this.indices = [];
-	 this.normals = [];
-	 this.originalTexCoords = [];
+ 
+ 	this.vertices = [];
+ 	var z = 0;
 
-	 var r = this.bottomRad;
-	 var delta_r = (this.topRad-this.bottomRad) / this.stacks;
-	 var delta_rad = 2*Math.PI/this.slices;
-	 var delta_z = this.height / this.stacks;
-	 var m = this.height/(this.bottomRad - this.topRad);
-	 var maxheight;
-	 if(this.bottomRad > this.topRad)
-	 	maxheight = this.topRad*m+this.height;
-	 else maxheight = this.bottomRad*m+this.height;
-	 var indice = 0;
-	 
-	 var acc = 0;
-	 for(var i = 0; i <= this.stacks; i++){
-	 	for(var j= 0; j <= this.slices; j++){
-	   	this.vertices.push(
-		r * Math.cos(j*delta_rad),
-		r * Math.sin(j*delta_rad), 
-		i*delta_z
-		);
-	   if(Math.abs(this.bottomRad - this.topRad) < 0.0001){
-		this.normals.push(
-		Math.cos(j*delta_rad),
-		Math.sin(j*delta_rad), 
-		0);
-	   }
-	   else if(this.bottomRad > this.topRad){
-		this.normals.push(
-		maxheight * Math.cos(j*delta_rad)/Math.sqrt(Math.pow(this.bottomRad, 2) + Math.pow(maxheight, 2)),
-		maxheight * Math.sin(j*delta_rad)/Math.sqrt(Math.pow(this.bottomRad, 2) + Math.pow(maxheight, 2)), 
-		this.bottomRad/Math.sqrt(Math.pow(this.bottomRad, 2) + Math.pow(maxheight, 2))
-		);
-	   }
-	   else{
-		this.normals.push(
-		maxheight * Math.cos(j*delta_rad)/Math.sqrt(Math.pow(this.topRad, 2) + Math.pow(maxheight, 2)),
-		maxheight * Math.sin(j*delta_rad)/Math.sqrt(Math.pow(this.topRad, 2) + Math.pow(maxheight, 2)), 
-		this.topRad/Math.sqrt(Math.pow(this.topRad, 2) + Math.pow(maxheight, 2))
-		);
-	   }
-	   this.originalTexCoords.push(j/this.slices, i/this.stacks);
-	   
-	  }
-	  r = (i+1) * delta_r + this.bottomRad;
-	 }
+ 	var radInterval = (this.topRadius - this.bottomRadius) / this.stacks;
+ 	var currentRad = this.bottomRadius;
 
-	 for(var i = 0; i < this.stacks; i++){
-	  acc = 0;
-	  for(var j = 0; j < this.slices; j++){
-		this.indices.push(
-		 i*(this.slices+1)+j,
-		 i*(this.slices+1)+(j+1),
-		 (i+1)*(this.slices+1)+(j+1)
-		 );
-		this.indices.push(
-		 (i+1)*(this.slices+1)+(j+1),
-		 (i+1)*(this.slices+1)+j,
-		 i*(this.slices+1)+j
-		 );
+ 	this.texCoords = [];
 
-	  }
-	 }
+ 	this.texelXInterval = 1/(this.slices);
+ 	this.texelYInterval = 1/(this.stacks);
 
-	 this.texCoords = this.originalTexCoords.slice();
+ 	this.texelY = 1;
+ 	
+ 	for (var n = 0; n <= this.stacks; z+=(1/this.stacks)*this.height) {
+ 	    var tempAngle = 0;
+ 	    this.texelX = 0;
+ 	    for (var i = 0; i <= this.slices; i++) {
+ 	    	if(i < this.slices){
+				this.vertices.push(Math.cos(tempAngle)*currentRad, Math.sin(tempAngle)*currentRad, z);
+				this.texCoords.push(this.texelX, this.texelY);
+ 	    	} else{
+ 	    		this.vertices.push(currentRad, 0, z);
+				this.texCoords.push(1, this.texelY);
+ 	    	}
 
-	 this.primitiveType = this.scene.gl.TRIANGLES;
-	 this.initGLBuffers();
+			this.texelX += this.texelXInterval;
+ 	        tempAngle += this.angle;
+ 	    };
+ 	    this.texelY -= this.texelYInterval;
+ 	    currentRad += radInterval;
+ 	    n++;
+ 	};
+
+ 	this.indices = [];
+
+	for(i = 0; i < ((this.slices*this.stacks)+(this.stacks-1)); i+=1){
+ 		if((i%(this.slices+1)) != (this.slices)){
+ 			this.indices.push(i+1, i + this.slices + 2, i + this.slices + 1);
+ 			this.indices.push(i,i+1, i + this.slices + 1);
+ 		}else{
+ 			this.indices.push(i+this.slices + 2, i+this.slices+1, i);
+ 			this.indices.push(i+this.slices + 2, i, i+1);
+ 		}
+ 	};
+
+	this.normals = [];
+
+ 	for (var z = 0; z <= this.stacks; z++){
+ 	    var tempAngle = 0;
+ 	    for (var i = 0; i <= this.slices; i++) {
+ 	        this.normals.push(Math.cos(tempAngle), Math.sin(tempAngle), 0);
+ 	        tempAngle += this.angle;
+  	    };
+ 	};
+
+ 	this.primitiveType = this.scene.gl.TRIANGLES;
+ 	this.initGLBuffers();
  };
 
-
-Cylinder.prototype.scaleTexCoords = function(ampS, ampT) {
-	for (var i = 0; i < this.texCoords.length; i += 2) {
-			this.texCoords[i] = this.originalTexCoords[i] / ampS;
-			this.texCoords[i + 1] = this.originalTexCoords[i+1] / ampT;
-	}
-	this.updateTexCoordsGLBuffers();
-}
+/*
+ * updateTextelCoordinates
+ * No need to update the textel's coordinates according to amplifS and amplifT.
+ *
+ * @param amplifS amplification factor s
+ * @param amplifT amplification factor t
+ */
+Cylinder.prototype.scaleTexCoords = function (amplifS, amplifT) {};
--- project1/parser/MySphere.js
+++ project1/parser/MySphere.js
@@ -1,58 +1,73 @@
-/**
+/*
  * MySphere
  * @constructor
+ * @param scene
+ * @param arguments [radius, stacks, slices]
  */
-  function MySphere(scene, radius, slices, stacks) {
+ function MySphere(scene, arg1, arg2, arg3) {
  	CGFobject.call(this,scene);
 	
-	this.radius=radius;
-	this.slices=slices;
-	this.stacks=stacks;
+	this.slices=arg1;
+	this.stacks=arg2;
+	this.radius=arg3;
+
+	this.angle=2*Math.PI/this.slices;
 
  	this.initBuffers();
  };
 
  MySphere.prototype = Object.create(CGFobject.prototype);
+
  MySphere.prototype.constructor = MySphere;
 
  MySphere.prototype.initBuffers = function() {
 
-	this.indices = [];
- 	this.vertices = [];
+ 	this.texCoords = [];
+
  	this.normals = [];
- 	this.originalTexCoords = [];
 
- 	var dTheta = Math.PI / this.stacks;
- 	var dPhi = 2 * Math.PI / this.slices;
+ 	this.texelXInterval = 1/(this.slices);
+ 	this.texelYInterval = 1/(this.stacks);
 
-	for (var stack = 0; stack <= this.stacks; ++stack) {
-		for (var slice = 0; slice <= this.slices; ++slice) {
-			this.vertices.push(this.radius * Math.sin(stack * dTheta) * Math.cos(slice * dPhi), this.radius * Math.sin(stack * dTheta) * Math.sin(slice * dPhi), this.radius * Math.cos(stack * dTheta));
-			this.normals.push(Math.sin(stack * dTheta) * Math.cos(slice * dPhi), Math.sin(stack * dTheta) * Math.sin(slice * dPhi), Math.cos(stack * dTheta));
-			this.originalTexCoords.push(slice/this.slices, 1-stack/this.stacks);
-		}
-	}
+	this.texelY = 0;
 
-	for (var stack = 0; stack < this.stacks; ++stack) {
-		for (var slice = 0; slice < this.slices; ++slice) {
-			this.indices.push(stack * (this.slices + 1) + slice, (stack + 1) * (this.slices + 1) + slice, (stack + 1) * (this.slices + 1) + slice + 1);
-			this.indices.push(stack * (this.slices + 1) + slice, (stack + 1) * (this.slices + 1) + slice + 1, stack * (this.slices + 1) + slice + 1);
-		}
-	}
+ 	this.vertices = [];
+ 	
+ 	var alpha = -Math.PI/2;
+ 	for (var n = 0; n <= this.stacks;n++, alpha+=(Math.PI/(this.stacks))) {
+ 	    var tempAngle = 0;
+ 	    this.texelX = 0;
+  	 	for (var i = 0; i <= this.slices; i++) {
+  			this.vertices.push(this.radius*Math.cos(alpha)*Math.cos(tempAngle), this.radius*Math.sin(alpha), this.radius*Math.cos(alpha)*Math.sin(tempAngle));
+  			this.normals.push(Math.cos(alpha)*Math.cos(tempAngle),  Math.sin(alpha), Math.cos(alpha)*Math.sin(tempAngle));
+ 	       	this.texCoords.push(this.texelX, this.texelY);
+  	 		this.texelX += this.texelXInterval;
+  	 		tempAngle += this.angle;
+ 	    };
+   	    this.texelY += this.texelYInterval;
+ 	};
 
-	this.texCoords = this.originalTexCoords.slice();
+ 	this.indices = [];
+
+	for(i = 0; i < ((this.slices*this.stacks)+(this.stacks-1)); i+=1){
+ 		if((i%(this.slices+1)) != (this.slices)){
+ 			this.indices.push(i + this.slices + 2, i+1, i + this.slices + 1);
+ 			this.indices.push(i+1, i, i + this.slices + 1);
+ 		}else{
+ 			this.indices.push(i+this.slices + 2, i, i+this.slices+1);
+ 			this.indices.push(i+this.slices + 2, i+1, i);
+ 		}
+ 	};
 
  	this.primitiveType = this.scene.gl.TRIANGLES;
  	this.initGLBuffers();
  };
 
- 
-MySphere.prototype.scaleTexCoords = function(ampS, ampT) {
-	
-	for (var i = 0; i < this.texCoords.length; i += 2) {
-			this.texCoords[i] = this.originalTexCoords[i] / ampS;
-			this.texCoords[i + 1] = this.originalTexCoords[i+1] / ampT;
-	}
-
-	this.updateTexCoordsGLBuffers();
-}
+/*
+ * updateTextelCoordinates
+ * No need to update the textel's coordinates according to amplifS and amplifT.
+ *
+ * @param amplifS amplification factor s
+ * @param amplifT amplification factor t
+ */
+MySphere.prototype.scaleTexCoords = function (amplifS, amplifT){};
