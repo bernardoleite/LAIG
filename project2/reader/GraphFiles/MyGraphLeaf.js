4af74ccf7016d5b8eeec69ec9cb99156e0333959
francisjssilva <francisjssilva@gmail.com>
2017-11-07 11:28:51 +0000

    added proj2
    added proj2

---
+++ project2/reader/GraphFiles/MyGraphLeaf.js
@@ -0,0 +1,41 @@
+/**
+ * MyGraphLeaf class, representing a leaf in the scene graph.
+ * @constructor
+**/
+function MyGraphLeaf(graph, nodeID, name, args, args2) {
+    this.graph = graph;
+    this.id = nodeID;
+    this.args = args;
+
+    
+    if(name == 'rectangle'){
+        this.type = new MyQuad(this.graph.scene, args);
+    }
+    else if(name == 'triangle'){
+        this.type = new MyTriangle(this.graph.scene, args);
+    }
+    else if(name == 'cylinder'){
+        this.type = new MyCylinderWithTamps(this.graph.scene, args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+    }
+    else if(name == 'sphere'){
+        this.type = new MySphere(this.graph.scene, args[0], args[1], args[2]);
+    }
+    else if(name == 'patch'){
+        this.partsU = this.args[0];
+        this.partsV = this.args[1];
+        this.degree1 = args2[0];
+        this.degree2 = args2[1];
+        this.cp = args2[2];
+
+
+        this.type = new Patch(this.graph.scene, this.degree1, this.degree2, this.partsU, this.partsV, this.cp);
+
+    }
+}
+
+
+
+
+
+
+
---
+++ project2/reader/Primitives/MyTriangle.js
@@ -0,0 +1,154 @@
+/**
+ * MyTriangle
+ * @param {attribues} [x1,y1,z1,x2,y2,z2,x3,y3,z3]
+ */
+function MyTriangle(scene, args) {
+	CGFobject.call(this,scene);
+
+	this.ampS = 1, this.ampT = 1;
+
+	this.x1 = args[6]; this.y1 = args[7]; this.z1 = args[8];
+
+	this.x2 = args[0]; this.y2 = args[1]; this.z2 = args[2];
+
+	this.x3 = args[3]; this.y3 = args[4]; this.z3 = args[5];
+
+	this.calculateVectors(); //Calculate Vertex Vectors in order to perfom calculations with cos's
+	this.calculateAngs(); //Calculate Angs to Know the Sum
+
+	this.initBuffers();
+};
+
+
+
+MyTriangle.prototype = Object.create(CGFobject.prototype);
+MyTriangle.prototype.constructor=MyTriangle;
+
+
+/**
+ * Creates New Triangle
+ */
+MyTriangle.prototype.initBuffers = function () {
+
+
+	this.vertices = [
+				this.x1, this.y1, this.z1,
+				this.x2, this.y2, this.z2,
+				this.x3, this.y3, this.z3
+			];
+
+	this.indices = [0,1,2];
+    
+	this.calculateNormalVectors(); //Calculate the Norms in relation to the triangle
+		
+	this.normals = [
+    this.norm1, this.norm2, this.norm3,
+    this.norm1, this.norm2, this.norm3,
+    this.norm1, this.norm2, this.norm3
+    ];
+	
+
+	
+	this.texCoords = 
+	[
+	  (this.dc - this.da * Math.cos(this.AngBeta)) / this.ampS, this.ampT - ((this.da * Math.sin(this.AngBeta)) /this.ampT),
+	  0.0, 
+	  1.0,
+	  this.dc / this.ampS, 
+	  1.0
+    ];
+
+	this.primitiveType=this.scene.gl.TRIANGLES;	
+	this.initGLBuffers();
+};
+
+
+/**
+ * Scale TexCoords using Amplifier S and Amplifier T
+ * @param {Number} amplifS 
+ * @param {Number} amplifT 
+ */
+MyTriangle.prototype.scaleTexCoords = function (amplifS, amplifT) {
+
+	this.texCoords = [	
+	  (this.dc - this.da * Math.cos(this.AngBeta)) / amplifS, 1 - ((this.da * Math.sin(this.AngBeta)) /amplifT),
+	  0.0, 
+	  1.0,
+	  this.dc / amplifS, 
+	  1.0
+    ];
+
+	this.updateTexCoordsGLBuffers();
+};
+
+/**
+ * Calculates the Vectors
+ */
+MyTriangle.prototype.calculateVectors = function () {
+
+	//Calculation of the args to put in the Squares
+	
+	this.Ccalc = (this.x3 - this.x2) * (this.x3 - this.x2) + 
+			 	(this.y3 - this.y2) * (this.y3 - this.y2) +
+			 	(this.z3 - this.z2) * (this.z3 - this.z2);
+
+	this.Acalc = (this.x1 - this.x3) * (this.x1 - this.x3) + 
+				(this.y1 - this.y3) * (this.y1 - this.y3) +
+			 	(this.z1 - this.z3) * (this.z1 - this.z3);
+
+	this.Bcalc = (this.x2 - this.x1) * (this.x2 - this.x1) + 
+			 	 (this.y2 - this.y1) * (this.y2 - this.y1) +
+			 	 (this.z2 - this.z1) * (this.z2 - this.z1);
+
+
+	//Squares of the a,b,c
+	this.da = Math.sqrt (this.Acalc);
+	this.db = Math.sqrt(this.Bcalc);
+	this.dc = Math.sqrt(this.Ccalc);
+	
+
+};
+
+/**
+ * Calculates Necessay Angles
+ */
+MyTriangle.prototype.calculateAngs = function () {
+
+	this.cbeta =  ( this.dc * this.dc + this.da*this.da - this.db*this.db);
+	this.cbeta = this.cbeta / (this.da * this.dc * 2);
+	
+	this.cgama = ( - this.dc * this.dc + this.da*this.da + this.db*this.db);
+	this.cgama = this.cgame / (this.da * this.db * 2);
+
+	this.calfa = (this.db*this.db + this.dc * this.dc - this.da*this.da);
+	this.calfa = this.calfa / (this.db * this.dc * 2);
+
+	this.AngAlfa = Math.acos(this.calfa); this.AngGama = Math.acos(this.cgama); this.AngBeta = Math.acos(this.cbeta);
+
+	this.totalSumAngs = this.AngBeta + this.AngAlfa + this.AngGama;
+	
+}
+
+/**
+ * Calculates Normals
+ */
+MyTriangle.prototype.calculateNormalVectors = function(){
+
+	var vetx1 = this.x2 - this.x1, vety1 = this.y2 - this.y1, vetz1 = this.z2 - this.z1;
+	var vetx2 = this.x3 - this.x1, vety2 = this.y3 - this.y1, vetz2 = this.z3 - this.z1;
+
+	this.norm1 = Math.abs(
+	vety1 * vetz2 - 
+	vetz1 * vety2
+	);
+
+	this.norm2 = Math.abs(
+	vetz1 * vetx2 - 
+	vetx1 * vetz2
+	);
+
+	this.norm3 = Math.abs(
+	vetx1 * vetx2 - 
+	vety1 * vetx2
+	);
+}
