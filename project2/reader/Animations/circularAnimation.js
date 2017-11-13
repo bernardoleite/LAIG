6a78e9949fcc6d546bb68349cb65e8ea0bf0eb69
francisjssilva <francisjssilva@gmail.com>
2017-11-13 16:53:40 +0000

    added update functions in circularAnimation and linearAnimation
    added update functions in circularAnimation and linearAnimation

--- project2/reader/Animations/circularAnimation.js
+++ project2/reader/Animations/circularAnimation.js
@@ -13,6 +13,25 @@ class circularAnimation extends animation{
 		this.startang = startang;
 		this.rotang = rotang;
 
+		this.w = speed/rotang;
+		this.currentAnimationAngle = 0;
+		this.transformMatrix = mat4.create();
+
 		console.warn(this);
-	}    
+	}
+
+	circularAnimation.prototype.update = function(dt) {
+    if(this.currentAnimationAngle < this.rotang){
+
+		var alpha = (this.w * dt) + this.startang;
+
+		this.currentAnimationAngle += alpha;
+
+		mat4.identity(this.transformMatrix);
+		this.transformMatrix.translate(this.centerx,this.centery,0);
+		this.transformMatrix.rotate(alpha, 0, 1, 0);
+		this.transformMatrix.translate(this.radius,0,0);
+		this.transformMatrix.rotate(90, 0, 1, 0);
+		}
+	}
 }
--- project2/reader/Animations/linearAnimation.js
+++ project2/reader/Animations/linearAnimation.js
@@ -2,8 +2,43 @@ class linearAnimation extends animation{
 	constructor(graph, animationID, animationType, speed, animationControlPoints) {
 	    super(graph, animationID, animationType, speed);
 
-	    this.graph = graph;
-	    this.controlPoints = animationControlPoints;
-	    console.warn(this);
+		this.graph = graph;
+		this.initValues = new Array();
+		this.currentAnimation = 0;
+
+		for (var i = 0; i < controlPoints.length; i++){
+			var values = new Array();
+
+			var distance = Math.sqrt(
+			(controlPoints[i+1][0] - controlPoints[i][0])*(controlPoints[i+1][0] - controlPoints[i][0]) +
+			(controlPoints[i+1][1] - controlPoints[i][1])*(controlPoints[i+1][1] - controlPoints[i][1]) +
+			(controlPoints[i+1][2] - controlPoints[i][2])*(controlPoints[i+1][2] - controlPoints[i][2]));
+
+			var cosAlfa = (controlPoints[i+1][0] - controlPoints[i][0])/distance;
+			var senAlfa = (controlPoints[i+1][1] - controlPoints[i][1])/distance;
+			var alfa = Math.acos(cosAlfa);
+			values.push([animationVelocity * cosAlfa], animationVelocity * senAlfa, alfa])
+			initValues.push(values);
+		}
+
+		this.transformMatrix = mat4.create();
+		this.time = 0;
+	}
+
+	linearAnimation.prototype.update = function(dt) {
+    if(this.currentAnimation < this.controlPoints.length - 1){
+
+		this.time += dt;
+		var dx = this.time * this.initValues[this.currentAnimation][0];
+		var dy = this.time * this.initValues[this.currentAnimation][1];
+
+		if (dx == this.controlPoints[this.currentAnimation][0] &&  dy == this.controlPoints[this.currentAnimation][1]) // currentAnimation has ended
+			this.currentAnimation++;
+
+		mat4.identity(this.transformMatrix);
+		this.transformMatrix.translate(dx,dy,0);
+		this.transformMatrix.translate(this.controlPoints[this.currentAnimation][0],this.controlPoints[this.currentAnimation][1],0);
+		this.transformMatrix.rotate(Math.acos(this.initValues[this.currentAnimation][3]), 0, 1, 0);
+		}
 	}
 }
